f052a46c8793cfbfbd519596c2f26877ae2262fc
Christopher Guilherme Farias Barbosa <mcdgui473@gmail.com>
2017-04-10 10:46:05 -0300

    Index.php atualizada
    Adicionei funcionalidade a alguns botões

--- Projeto 1.9/bootstrap-3.3.7/docs/examples/jumbotron/buscar.php
+++ Projeto 1.9/bootstrap-3.3.7/docs/examples/jumbotron/buscar.php
@@ -27,10 +27,10 @@
   </div>
   <div id="navbar" class="navbar-collapse collapse">
    <ul class="nav navbar-nav navbar-right">
-    <li><a href="#">Início</a></li>
-    <li><a href="#">Opções</a></li>
-    <li><a href="#">Perfil</a></li>
-    <li><a href="#">Ajuda</a></li>
+    <li><a href="index.php">Início</a></li>
+    <li><a href="option.php">Opções</a></li>
+    <li><a href="perfil.php">Perfil</a></li>
+    <li><a href="ajuda.php">Ajuda</a></li>
    </ul>
   </div>
  </div>
--- Projeto 1.9/bootstrap-3.3.7/docs/examples/jumbotron/tabela.php
+++ Projeto 1.9/bootstrap-3.3.7/docs/examples/jumbotron/tabela.php
@@ -38,7 +38,7 @@
 <hr>
 <div id="main" class="container-fluid">
 <br><br>
-
+</div>
 <!-- Scripts do Bootstrap usados aqui -->
  <script src="js/jquery.min.js"></script>
  <script src="js/bootstrap.min.js"></script>
