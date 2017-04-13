541a6bf25521b1457e285139afe410ae38af7660
Mr Mathias(Matheus Giovani) <www.matheusgiovane@hotmail.com>
2017-04-13 10:28:17 -0300

    dajobu

---
+++ Projeto 1.9.2/bootstrap-3.3.7/docs/examples/jumbotron/buscar.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Projeto X - Visualizar</title>
+ <link rel="icon" href="logo.png">
+ <!-- Link com todos os CSS relacionados a está página -->
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+ <link rel="stylesheet" type="text/css" href="estilo_buscar.css">
+ <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body> 
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+ <div class="container-fluid">
+  <div class="navbar-header">
+   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Barra de navegação</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+   </button>
+   <a class="navbar-brand" href="#">Projeto X - Buscar</a>
+   <img src="eye.png" class="icone">
+  </div>
+  <div id="navbar" class="navbar-collapse collapse">
+   <ul class="nav navbar-nav navbar-right">
+    <li><a href="#">Início</a></li>
+    <li><a href="#">Opções</a></li>
+    <li><a href="#">Perfil</a></li>
+    <li><a href="#">Ajuda</a></li>
+   </ul>
+  </div>
+ </div>
+</nav>
+<hr>
+<div id="main" class="container-fluid">
+<br><br>
+ <form action="editar_go.php" method="POST">
+<?php
+	include('database.php');
+	$buscarDados = $pdo -> query("SELECT * FROM usuarios");
+	while ($dados = $buscarDados->fetch(PDO::FETCH_ASSOC)) {
+//Aqui está os echo's responsaveis por mostrar os dados ao usuário
+    echo "<table border='0px'>";
+    echo "<tr>";
+		echo "<td class='itens'>ID: ".$dados['id_user']."</td>";
+		echo "<td class='itens'>Nome: ".$dados['nome']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+		echo "<td class='itens'>Email: ".$dados['email']."</td>";
+		echo "<td class='itens'>Senha: ".$dados['senha']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+		echo "<td class='itens'>Cargo: ".$dados['cargo']."</td>";
+		echo "<td class='itens'>Setor: ".$dados['setor']."</td>";
+    echo "</tr>";
+    echo "<tr>";
+		echo "<td class='itens'>RG: ".$dados['rg']."</td>";
+    echo "</tr>";
+    echo "</table>";
+//Aqui está os botões responsaveis por levar os dados as páginas de deletar ou alterar informções dos usuários
+		echo "<br><a href='deletar.php?id_user=".$dados['id_user']."' class='btn btn-primary'>Deletar</a> <a href='alterar.php?id_user=".$dados['id_user']."'  class='btn btn-primary'>Alterar</a>";
+		echo "<hr>";
+	}
+
+?>
+</form>
+<!-- Scripts do Bootstrap usados aqui -->
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
