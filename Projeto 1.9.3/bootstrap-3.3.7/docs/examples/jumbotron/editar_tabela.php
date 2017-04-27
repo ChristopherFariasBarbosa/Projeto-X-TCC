a2e92f58a035db2e4d34744967257758d33d8e83
Mr Mathias(Matheus Giovani) <www.matheusgiovane@hotmail.com>
2017-04-27 11:14:17 -0300

    testa ae ³

--- Projeto 1.9.3/bootstrap-3.3.7/docs/examples/jumbotron/editar_tabela.php
+++ Projeto 1.9.3/bootstrap-3.3.7/docs/examples/jumbotron/editar_tabela.php
@@ -3,6 +3,6 @@
 	$id = $_POST['id_item'];
 	$modelo = $_POST['modelo'];
 	$descricao = $_POST['descricao'];
-	$editarDados = $pdo -> query("UPDATE itens SET modelo='".$modelo."', descricao='".$descricao."' ");
+	$editarDados = $pdo -> query("UPDATE itens SET modelo='".$modelo."', descricao='".$descricao."' WHERE id_item = '".$id."' ");
 	header('Location:tabela.php');
 ?>
